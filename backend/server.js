815e290cfb8a4c5413e96a11c11e565a82e32bf1
Neggev <66245646+Neggev@users.noreply.github.com>
2022-07-10 15:56:00 +0300

    prod-added emgines at backend/package.json and removed cors from server

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ const PORT = process.env.PORT || 8000;
 
 const buildPath = path.join(__dirname, '..', 'build')
 
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(buildPath));
