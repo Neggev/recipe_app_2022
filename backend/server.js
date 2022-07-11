6632ad4b6e96979f601c2da1801cf4aa9dea122a
Neggev <66245646+Neggev@users.noreply.github.com>
2022-07-11 19:25:59 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,9 @@ const PORT = process.env.PORT || 8000;
 
 const buildPath = path.join(__dirname, '..', 'build')
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(buildPath));
